53bb684fc3f476666bebff7b01e8b5dba9ed22be
Carlos Philipe Mendes Bahia <carlosphbahia@gmail.com>
2022-07-25 14:49:25 -0300

    feat: Start

---
+++ PhrasesOfDay/PhrasesOfDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhrasesOfDay
+//
+//  Created by Carlos Philipe Mendes Bahia on 25/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
